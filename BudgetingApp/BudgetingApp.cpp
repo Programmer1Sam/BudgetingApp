a07d48d8c26a8babe95affa9e2dafdb95664af5e
thatoneproto <81997712+thatoneproto@users.noreply.github.com>
2022-12-13 14:56:13 -0600

    All current errors patched.

--- BudgetingApp/BudgetingApp.cpp
+++ BudgetingApp/BudgetingApp.cpp
@@ -82,9 +82,8 @@ string GetAccountName() {
 		getline(loadFile, garbageInput);
 
 		loadFile.clear();
-		loadFile.ignore();
 
-		loadFile >> accountName;
+		getline(loadFile, accountName);
 		loadFile.close();
 	}
 	catch (runtime_error& excpt) {
@@ -149,6 +148,8 @@ void LoadFile(vector<string>& namesOfEntries, vector<double>& entries) {
 
 		getline(cin, garbageData);
 		getline(cin, garbageData);
+		getline(cin, garbageData);
+		getline(cin, garbageData);
 		
 		while (loadData >> name >> entry) {
 			namesOfEntries.push_back(name);
@@ -167,7 +168,10 @@ void LoadFile(vector<string>& namesOfEntries, vector<double>& entries) {
 ********************************/
 void SaveFile(vector<string> namesOfEntries, vector<double> entries) {
 	ofstream saveDataToFile;
+	
+	double beginningTotal = GetTotal();
 
+	string accountName = GetAccountName();
 	try {
 		saveDataToFile.open("BudgetList.txt");
 
@@ -175,8 +179,13 @@ void SaveFile(vector<string> namesOfEntries, vector<double> entries) {
 			throw runtime_error("File unable to be opened.");
 		}
 
-		saveDataToFile << GetTotal() << endl;
-		saveDataToFile << GetAccountName() << endl;
+		saveDataToFile << beginningTotal << endl;
+		saveDataToFile << accountName << endl;
+
+		for (int i = 0; i < accountName.size(); i++) {
+			saveDataToFile << "-";
+		}
+		saveDataToFile << endl;
 
 		for (int i = 0; i < namesOfEntries.size() && i < entries.size(); i++) {
 			saveDataToFile << namesOfEntries.at(i) << " " << entries.at(i) << endl;
